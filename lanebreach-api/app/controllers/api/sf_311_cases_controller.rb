877685442fa753853f3498fd4a39b04ec5618db6
Alex Wood <alex.wood.86@gmail.com>
2018-11-16 16:42:49 -0800

    + create and read functionality for 311 reports

--- lanebreach-api/app/controllers/api/sf_311_cases_controller.rb
+++
@@ -1,8 +0,0 @@
-class Api::Sf311CasesController < ApplicationController
-  def index
-    client = Sf311CasesService.new
-    data = client.get_data
-    render json: data.to_json, status: :ok
-  end
-end
-    
