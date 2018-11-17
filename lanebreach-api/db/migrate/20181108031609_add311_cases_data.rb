0ff5866c649c5267b95dd271a7d5bf14129ca38e
Alex Wood <alex.wood.86@gmail.com>
2018-11-16 16:45:20 -0800

    + missing 311 case fields

--- lanebreach-api/db/migrate/20181108031609_add311_cases_data.rb
+++
@@ -1,9 +0,0 @@
-class Add311CasesData < ActiveRecord::Migration[5.2]
-  def change
-    create_table :sf_311_cases do |t|
-      t.string :status_description
-      t.string :police_district
-      t.timestamps      
-    end
-  end
-end
