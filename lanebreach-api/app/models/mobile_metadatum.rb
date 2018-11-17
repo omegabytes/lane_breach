1518ef25b92f98e8779e5d4993ecfdd1869cfc0b
Alex Wood <alex.wood.86@gmail.com>
2018-11-16 16:44:36 -0800

    + annotate gem for displaying model schemas in code
    https://github.com/ctran/annotate_models

--- lanebreach-api/app/models/mobile_metadatum.rb
+++ lanebreach-api/app/models/mobile_metadatum.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: mobile_metadata
+#
+#  id          :bigint(8)        not null, primary key
+#  category    :string
+#  environment :string
+#  token       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  request_id  :string
+#
+
 class MobileMetadatum < ApplicationRecord
 end
