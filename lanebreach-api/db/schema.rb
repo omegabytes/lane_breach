8733ab718770e4549dea802fc655d5e9347b8d8c
Alex Wood <alex.wood.86@gmail.com>
2018-12-12 17:11:37 -0800

    + index on requested datetime
    Most queries will include constraints against this field

--- lanebreach-api/db/schema.rb
+++ lanebreach-api/db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_03_035653) do
 ActiveRecord::Schema.define(version: 2018_12_12_224143) do
 
   # These are extensions that must be enabled in order to support this database
@@ -93,6 +92,7 @@ ActiveRecord::Schema.define(version: 2018_12_12_224143) do
     t.string "media_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["requested_datetime"], name: "index_sf311_cases_on_requested_datetime"
     t.index ["service_request_id"], name: "index_sf311_cases_on_service_request_id", unique: true
   end
 
